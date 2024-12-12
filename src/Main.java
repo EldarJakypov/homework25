22f85ade62f52aa3de0e2f9fdea3b1f01b01d451
Eldar Jakypov <eldar.nazar.02@gmail.com>
2024-12-12 18:58:03 +0600

    Modified Main to use different payment methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import util.CoinAcceptorImpl;
+import util.CardAcceptorImpl;
+import util.PaymentInterface;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        AppRunner.run();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Выберите способ оплаты:");
+        System.out.println("1 - Монеты");
+        System.out.println("2 - Карта");
+
+        int choice = scanner.nextInt();
+        PaymentInterface paymentMethod = null;
+
+        switch (choice) {
+            case 1:
+                paymentMethod = new CoinAcceptorImpl(0);
+                break;
+            case 2:
+                paymentMethod = new CardAcceptorImpl();
+                break;
+            default:
+                System.out.println("Неверный выбор. Программа завершена.");
+                return;
+        }
+        AppRunner.run(paymentMethod);
     }
 }
